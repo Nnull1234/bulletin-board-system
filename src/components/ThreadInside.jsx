a2876c67b7e1ed508900b7b5e2cd956f389bf723
hathat1212 <zinnsuke12@gmail.com>
2023-01-23 10:39:34 +0900

    学校で最終調整

--- src/components/ThreadInside.jsx
+++ src/components/ThreadInside.jsx
@@ -1,39 +1,40 @@
-import React from 'react'
-import { useState } from 'react';
+import React from "react";
+import { useState } from "react";
 const ThreadInside = (props) => {
-    const [threadContents, setThreadContents] = useState(null)
+  const [threadContents, setThreadContents] = useState(null);
+  const [id, setId] = useState([]);
 
-    const getThreadListData = () => {
-        fetch(
-            `https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads/${props.id}/posts?offset=0`,
-            { method: "GET" }
-        )
-            .then((res) => {
-                return res.json();
-            })
-            .then((data) => {
-                setThreadContents(data.posts);
-                console.log(threadContents);
-            })
-    }
-    return (
-        <div>
-            <button onClick={getThreadListData}>げと</button>
-
-            {
-                threadContents !== null ? (
-                    <ul>
-                        {threadContents.map((post) => (
-                            <div id="list" key={post.id}>
-                                {post.post}
-                            </div>
-                        ))}
-                    </ul>
-                ) : <p>投稿はありません</p>
-            }
-
-        </div>
+  const getThreadListData = () => {
+    fetch(
+      `https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads/${props.id}/posts?offset=0`,
+      { method: "GET" }
     )
-}
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        setThreadContents(data.posts);
+        setId(data.threadId);
+        console.log(id);
+      });
+  };
+  return (
+    <div>
+      <button onClick={getThreadListData}>げと</button>
 
-export default ThreadInside
+      {threadContents !== null ? (
+        <ul>
+          {threadContents.map((post) => (
+            <div id="list" key={post.id}>
+              {post.post}
+            </div>
+          ))}
+        </ul>
+      ) : (
+        <p>投稿はありません</p>
+      )}
+    </div>
+  );
+};
+
+export default ThreadInside;
